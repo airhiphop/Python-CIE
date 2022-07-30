c354916c0cc637ab5521ed50033ac91c960baf9d
airhiphop@163.com <airhiphop@163.com>
2022-07-31 07:39:38 +0800

    update

--- Level-1/2022-03/37.py
+++ Level-1/2022-03/37.py
@@ -1,16 +1,3 @@
-import turtle
-turtle.pensize(10)
-turtle.color('black', 'blue')
-turtle.begin_fill()
-turtle.circle(50)
-turtle.end_fill()
-turtle.forward(90)
-turtle.left(120)
-turtle.forward(180)
-turtle.goto(0,0)
-turtle.setheading(180)
-turtle.forward(90)
-turtle.right(120)
-turtle.forward(180)
+
     
         
